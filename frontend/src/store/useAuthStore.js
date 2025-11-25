188827b58a86f4105fea535c04ab1d933fb5567c
sohail-24 <mdsohail88008@gmail.com>
2025-11-25 14:08:02 +0530

    Refactor BASE_URL handling for environment support

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,10 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:5001" : "http://13.232.232.135:30001";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -103,4 +106,5 @@ export const useAuthStore = create((set, get) => ({
     if (get().socket?.connected) get().socket.disconnect();
   },
 }));
+
 
